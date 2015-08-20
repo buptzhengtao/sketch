aac528f562ec746c63a87acb8f8be03e721c6287
zhengtao <zhengtao@bjmac.local>
2015-08-20 16:47:33 +0800

    load dll

--- qos_manager.cpp
+++ qos_manager.cpp
@@ -6,6 +6,73 @@
 
 #include "netio/select/qos_manager.h"
 
+typedef BOOL (*QOSCreateHandleFunc)(QOS_VERSION*, HANDLE*);
+
+typedef BOOL (*QOSAddSocketToFlowFunc)(HANDLE, 
+                                    SOCKET,
+                                    sockaddr*,
+                                    QOS_TRAFFIC_TYPE,
+                                    DWORD,
+                                    ULONG*);
+
+typedef BOOL (*QOSRemoveSocketFromFlowFunc)(HANDLE, 
+                                         SOCKET,
+                                         ULONG,
+                                         DWORD);
+
+typedef BOOL (*QOSCloseHandleFunc)(HANDLE);
+
+static HINSTANCE QOSdllHandle = NULL;
+static QOSCreateHandleFunc _QOSCreateHandle = NULL;
+static QOSAddSocketToFlowFunc _QOSAddSocketToFlow = NULL;
+static QOSRemoveSocketFromFlowFunc _QOSRemoveSocketFromFlow = NULL;
+static QOSCloseHandleFunc _QOSCloseHandle = NULL;
+
+static int LoadLibQwave()
+{
+    QOSdllHandle = LoadLibrary("qwave.dll");
+
+    if ( QOSdllHandle == NULL ) {
+        return -1;
+    }
+
+    _QOSCreateHandle = 
+        (QOSCreateHandleFunc)GetProcAddress(
+            QOSdllHandle, 
+            "QOSCreateHandle");
+
+    _QOSAddSocketToFlow =
+        (QOSAddSocketToFlowFunc)GetProcAddress(
+            QOSdllHandle, 
+            "QOSAddSocketToFlow");
+
+    _QOSRemoveSocketFromFlow = 
+        (QOSRemoveSocketFromFlowFunc)GetProcAddress(
+            QOSdllHandle,
+            "QOSRemoveSocketFromFlow");
+
+    _QOSCloseHandle =
+        (QOSCloseHandleFunc)GetProcAddress(
+            QOSdllHandle,
+            "QOSCloseHandle");
+
+    if ( _QOSCreateHandle == NULL ||
+         _QOSAddSocketToFlow == NULL ||
+         _QOSRemoveSocketFromFlow == NULL ||
+         _QOSCloseHandle == NULL ) {
+        return -1;
+    }
+
+    return 0;
+}
+
+statuc void FreeLibQwave()
+{
+    if ( QOSdllHandle != NULL ) {
+        FreeLibrary(QOSdllHandle);
+    }
+}
+
 QOSManager* QOSManager::_instance = NULL;
 std::mutex QOSManager::_instance_mutex;
 
@@ -26,11 +93,15 @@ QOSManager::QOSManager() :
     _flow_id(0),
     _qos_handle(NULL)
 {
+    if ( LoadLibQwave() != 0 ) {
+        return;
+    }
+
     QOS_VERSION version;
     version.MajorVersion = 1;
     version.MinorVersion = 0;
 
-    if ( !QOSCreateHandle(&version, &_qos_handle) ) {
+    if ( !_QOSCreateHandle(&version, &_qos_handle) ) {
         _qos_handle = NULL;
         //DEBUG("Failed to create QOS handler, err(%d)", GetLastError());
     }
@@ -38,15 +109,15 @@ QOSManager::QOSManager() :
 
 QOSManager::~QOSManager()
 {
-    if ( _qos_handle == NULL ) {
-        return;
+    if ( _qos_handle != NULL ) {
+        if ( _flow_id != 0 ) {
+            _QOSRemoveSocketFromFlow(_qos_handle, NULL, _flow_id, 0);
+        }
+
+        _QOSCloseHandle(_qos_handle);
     }
 
-    if ( _flow_id != 0 ) {
-        QOSRemoveSocketFromFlow(_qos_handle, NULL, _flow_id, 0);
-    }
-
-    QOSCloseHandle(_qos_handle);
+    FreeLibQwave();
 }
 
 void QOSManager::add(SOCKET fd)
@@ -57,7 +128,7 @@ void QOSManager::add(SOCKET fd)
         return;
     }
 
-    BOOL ret = QOSAddSocketToFlow(
+    BOOL ret = _QOSAddSocketToFlow(
         _qos_handle, 
         fd,
         NULL,
@@ -78,6 +149,6 @@ void QOSManager::rmv(SOCKET fd)
         return;
     }
 
-    QOSRemoveSocketFromFlow(_qos_handle, fd, _flow_id, 0);
+    _QOSRemoveSocketFromFlow(_qos_handle, fd, _flow_id, 0);
 }
 
